ddcf38e84967e08370d841d9b7e60b58f118b951
Tptogiar <2528891112@qq.com>
2022-01-04 02:14:09 +0800

    JDK动态代理

---
+++ SpringAOPDemo/src/main/java/_01_DynamicProxy/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package _01_DynamicProxy;
+
+/**
+ * @author Tptogiar
+ * @description
+ * @date 2022/1/3 - 23:09
+ */
+public class UserServiceImpl implements UserService{
+
+
+    public void add(){
+        System.out.print(" add ");
+    }
+
+
+
+}
